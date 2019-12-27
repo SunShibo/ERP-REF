b45767990e239e3e6a774fa655ef203bf07b9350
xiaowen1024 <15214440324@163.com>
2019-12-27 13:06:52 +0800

    msg

--- src/main/java/com/wisewintech/erp/config/MybatisPlusConfig.java
+++ src/main/java/com/wisewintech/erp/config/MybatisPlusConfig.java
@@ -1,7 +1,6 @@
 package com.wisewintech.erp.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,10 +10,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableTransactionManagement
 @Configuration
-@MapperScan("com.wisewintech.*")
 public class MybatisPlusConfig {
+
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        return   new PaginationInterceptor();
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        return paginationInterceptor;
     }
 }
